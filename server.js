046f5ec77791692dddce1326c298b3c9ade14a43
historyunfiltered07-hub <historyunfiltered07@gmail.com>
2026-02-13 17:39:04 +0530

    Update server.js

--- server.js
+++ server.js
@@ -17,4 +17,5 @@ app.post('/chat', async (req, res) => {
     res.json({ reply: response.choices[0].message.content });
 });
 
-app.listen(3000, () => console.log('Server running on port 3000'));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
